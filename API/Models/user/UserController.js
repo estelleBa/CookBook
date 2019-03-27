5a8f43e62b7031fc878ceef930effe6d4634ce59
Your Name <you@example.com>
2019-03-27 12:38:50 +0100

    followers

--- API/Models/user/UserController.js
+++ API/Models/user/UserController.js
@@ -45,7 +45,7 @@ router.route('/create')
 				password: pass
 			});
 			newUser.save(function(err){
-				if(err) res.status(500).json({res : 500});
+				if(err) res.status(500).json({res : err});
 				else res.status(200).json({res : 200});
 			});
 		}
@@ -62,7 +62,7 @@ router.route('/find/:id')
   if(isLoggedIn(req)){
     var ObjectId = mongoose.Types.ObjectId(req.params.id);
     UserModel.User.findOne({ _id : ObjectId}).exec(function(err, doc){
-			if(err) res.status(500).json({res : 500});
+			if(err) res.status(500).json({res : err});
 			else if(!doc) res.status(404).json({res : 404});
 			else res.status(200).json({user : doc});
     });
@@ -80,13 +80,13 @@ router.route('/update/:id')
   if(user_id==ObjectId){
 		const body = req.body;
     UserModel.User.findOne({ _id : user_id}).exec(function(err, doc){
-			if(err) res.status(500).json({res : 500});
+			if(err) res.status(500).json({res : err});
 			if(!doc) res.status(404).json({res : 404});
 			else {
 				var login = (body.login !== undefined && body.login !== '') ? body.login : doc.login;
 				var email = (body.email !== undefined && body.email !== '') ? body.email : doc.email;
 				UserModel.User.findOneAndUpdate({ _id : user_id}, {$set: {login: login, email: email}}).exec(function(err, doc){
-					if(err) res.status(500).json({res : 500});
+					if(err) res.status(500).json({res : err});
 					else res.status(200).json({res : 200});
 	      });
 			}
@@ -103,11 +103,19 @@ router.route('/delete/:id')
 	const user_id = isLoggedIn(req);
 	const ObjectId = mongoose.Types.ObjectId(req.params.id);
   if(user_id==ObjectId){
-    UserModel.User.deleteOne({ _id : ObjectId}).exec(function(err, doc){
-			if(err) res.status(500).json({res : 500});
+    UserModel.User.deleteOne({ _id : user_id }).exec(function(err, doc){
+			if(err) res.status(500).json({res : err});
 			else {
 				req.session.destroy();
-				res.status(200).json({res : 200});
+				UserModel.User.updateMany(
+					{ },
+					{ $pull: { followings: user_id, followers: user_id } }
+				).exec(function(err, doc){
+					if(err) res.status(500).json({res : err});
+					else {
+						res.status(200).json({res : 200});
+					}
+				});
 			}
     });
   }
@@ -125,7 +133,7 @@ router.route('/login')
     if(body.login !== undefined && body.login !== '' &&
     body.password !== undefined && body.password !== '') {
       UserModel.User.findOne({ login: body.login }).exec(function(err, doc){
-				if(err) res.status(500).json({res : 500});
+				if(err) res.status(500).json({res : err});
 				if(!doc) errs.push('login');
         else if(doc){
           if(comparePass(body.password, doc.password)){
@@ -188,20 +196,38 @@ router.route('/follow')
 		const item_type = req.body.type;
 		switch(item_type){
 			case 'u':
-				UserModel.User.findOne({ _id : user_id }).exec(function(err, doc){
-					if(doc){
-						doc.followings.push(item_id);
-						doc.save();
-
-						UserModel.User.findOne({ _id : item_id }).exec(function(err, doc){
-							if(doc){
-								doc.followers.push(user_id);
-								doc.save();
+				UserModel.User.updateOne(
+					{ _id: user_id, followings: { $ne: item_id } },
+					{ $push: { followings: item_id } }
+				).exec(function(err, doc){
+					if(err) res.status(500).json({res : err});
+					else {
+						UserModel.User.updateOne(
+							{ _id: item_id, followers: { $ne: user_id } },
+							{ $push: { followers: user_id } }
+						).exec(function(err, doc){
+							if(err) res.status(500).json({res : err});
+							else {
+								res.status(200).json({res : 200});
 							}
 						});
 					}
-					res.status(200).json({res : 200});
 				});
+
+				// UserModel.User.findOne({ _id : user_id }).exec(function(err, doc){
+				// 	if(doc){
+				// 		doc.followings.push(item_id);
+				// 		doc.save();
+				//
+				// 		UserModel.User.findOne({ _id : item_id }).exec(function(err, doc){
+				// 			if(doc){
+				// 				doc.followers.push(user_id);
+				// 				doc.save();
+				// 			}
+				// 		});
+				// 	}
+				// 	res.status(200).json({res : 200});
+				// });
 				break;
 			case 'b':
 
@@ -222,16 +248,34 @@ router.route('/unfollow')
 	const user_id = isLoggedIn(req);
 	if(user_id){
 		const item_id = mongoose.Types.ObjectId(req.body.id);
-		UserModel.User.findOne({ _id: user_id }).exec(function(err, doc){
-			if(err) res.status(500).json({res : 500});
-			else if(!doc) res.status(404).json({res : 404});
-			else if(doc.follow.indexOf(item_id) != -1) {
-				doc.follow.pull(item_id);
-				doc.save();
-				UserModel.Follow.deleteOne({ user : user_id, item_id : item_id }).exec();
-			}
-			res.status(200).json({res : 200});
-		});
+		const item_type = req.body.type;
+		switch(item_type){
+			case 'u':
+				UserModel.User.updateOne(
+					{ _id: user_id },
+					{ $pull: { followings: item_id } }
+				).exec(function(err, doc){
+					if(err) res.status(500).json({res : err});
+					else {
+						UserModel.User.updateOne(
+							{ _id: item_id },
+							{ $pull: { followers: user_id } }
+						).exec(function(err, doc){
+							if(err) res.status(500).json({res : err});
+							else {
+								res.status(200).json({res : 200});
+							}
+						});
+					}
+				});
+				break;
+			case 'b':
+
+				break;
+			case '#':
+
+				break;
+		}
 	}
 	else res.status(401).json({res : 401});
 });
@@ -244,7 +288,7 @@ router.route('/followed')
 	const user_id = isLoggedIn(req);
 	if(user_id){
 		UserModel.User.findOne({ _id: user_id }).populate('followings').exec(function(err, doc){
-			if(err) res.status(500).json({res : 500});
+			if(err) res.status(500).json({res : err});
       else res.status(200).json({res : doc});
     });
 	}
